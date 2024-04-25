907b89d0ecd4c2ede27625b1336e6c1dbb53f0c8
rawrrawrpurpledinosaur <191292L@students.hci.edu.sg>
2024-04-25 16:17:01 +0800

    moderation cmds added

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 import sqlite3 
 import random
 from py import capy
+from dotenv import load_dotenv
 
 conn = sqlite3.connect("database.db")
 c = conn.cursor()
@@ -19,7 +20,7 @@ guild_ids = []
 async def on_ready():
     print(f"Logged in as {bot.user}")
     print(capy)
-
+    await bot.change_presence(activity=discord.Game(name="capy help"))
     # check if guild.id is in database, else add to db
     for guild in bot.guilds:
         exisiting_guild_ids = c.execute("SELECT guild_id FROM guilds").fetchall()
@@ -60,11 +61,25 @@ async def purge(ctx, amount: int):
     await ctx.channel.purge(limit=amount+1)
     await ctx.send(f"{ctx.author.mention} purged {amount} messages")
 
+@purge.error
+async def purge_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please specify an amount of messages to delete")
+    elif isinstance(error, commands.MissingPermissions):
+        await ctx.send("You do not have permission to do that")
+
 @bot.command()
 @commands.has_permissions(kick_members=True)
 async def kick(ctx, member: discord.Member, *, reason="No reason provided"):
     await member.kick(reason=reason)
     await ctx.send(f"{member.mention} has been kicked")
+
+@kick.error
+async def kick_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please specify a member to kick")
+    elif isinstance(error, commands.MissingPermissions):
+        await ctx.send("You do not have permission to do that")
 
 @bot.command()
 @commands.has_permissions(ban_members=True)
@@ -72,15 +87,57 @@ async def ban(ctx, member: discord.Member, *, reason="No reason provided"):
     await member.ban(reason=reason)
     await ctx.send(f"{member.mention} has been banned")
 
-# Handles errors 
+@ban.error
+async def ban_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please specify a member to ban")
+    elif isinstance(error, commands.MissingPermissions):
+        await ctx.send("You do not have permission to do that")
+
+
+@bot.command()
+async def softban(ctx, member: discord.Member, reason = "No reason provided"):
+    await member.ban(reason=reason)
+    await member.unban(reason="Softban")
+    await ctx.send(f"{member.mention} has been softbanned")
+    await ctx.channel.purge(limit=100, check=lambda m: m.author == member)
+
+@softban.error
+async def softban_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please specify a member to softban")
+    elif isinstance(error, commands.MissingPermissions):
+        await ctx.send("You do not have permission to do that")
+
+@bot.command() 
+async def unban(ctx, *, member):
+    banned_users = ctx.guild.bans()
+    async for ban_entry in banned_users:
+        user = ban_entry.user
+        if user.name == member:
+            await ctx.guild.unban(user)
+            await ctx.send(f"{user.mention} has been unbanned")
+            return
+
+# Handles errors
 @bot.event 
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         await ctx.send("Command not found")
-    else:
-        await ctx.send("An error occured")
 
-with open("token.txt") as f:
-    TOKEN = f.read()
+class MyNewHelp(commands.MinimalHelpCommand):
+    async def send_pages(self):
+        destination = self.get_destination()
+        for page in self.paginator.pages:
+            emby = discord.Embed(description=page)
+            await destination.send(embed=emby)
 
+bot.help_command = MyNewHelp()
+
+# Gambling commands
+@bot.command()
+async def coinflip(ctx, bet: int):
+    pass
+load_dotenv()
+TOKEN = os.getenv("TOKEN") 
 bot.run(TOKEN)
